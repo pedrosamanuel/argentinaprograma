d752019fff8305f0d0c4b6639c9683c91c870645
pedrosa <mpedrosa2110@gmail.com>
2023-03-02 20:14:14 -0300

    ap

--- curso-java/src/ordenamiento.java
+++ curso-java/src/ordenamiento.java
@@ -1,5 +1,7 @@
 public class ordenamiento {
     public static void main(String[] args){
+        //realizamos tres algoritmos de ordenamiento y comparamos sus tiempos en nanosegundos
+
         long startTime = System.nanoTime();
         sortBubble();
         long  endTime = (System.nanoTime() - startTime);
@@ -8,7 +10,10 @@ public class ordenamiento {
         betterBubble();
         long  endTime1 = (System.nanoTime() - startTime1) ;
         System.out.println(endTime1);
-
+        long startTime2 = System.nanoTime();
+        sortBubble1();
+        long  endTime2 = (System.nanoTime() - startTime2);
+        System.out.println(endTime2);
     }
     public static void sortBubble(){
         int aux;
@@ -46,6 +51,25 @@ public class ordenamiento {
             System.out.println(ordenados[n]);
             n++;
         }
+    }
+    public static void sortBubble1(){
+        int aux;
+        int ordenados[] = {9,13,22,4,15,3,17,2,1};
+        for(int i = 0 ; i < ordenados.length;i++) {
+            for (int j = 0; j < ordenados.length-1 ;j++){
+                if (ordenados[j] < ordenados[j + 1]) {
+                    aux = ordenados[j];
+                    ordenados[j] = ordenados[j + 1];
+                    ordenados[j + 1] = aux;
+                }
+            }
+        }
+        int n = 0;
+        while(n < ordenados.length){
+            System.out.println(ordenados[n]);
+            n++;
+        }
+
     }
 
 
